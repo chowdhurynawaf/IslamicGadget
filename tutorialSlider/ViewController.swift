5c25e34d37702d5aef9ce388d67487d338681f54
nawafChowdhuryAkash <nawafc959@gmail.com>
2020-02-07 22:57:50 +0600

    second and third screen skip button is not showing resonse

---
+++ tutorialSlider/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  tutorialSlider
+//
+//  Created by as on 2/6/20.
+//  Copyright © 2020 as. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func skipBtnTappd(_ sender: Any) {
+        
+        print("skipBtnTapped")
+        
+    }
+    
+
+}
+
